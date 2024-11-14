8adf29ea88a65e3b57f2f868ada1d0764ec77a3d
Arndy345 <nonsoandrew1@gmail.com>
2024-11-14 08:48:14 +0100

    Server Up and running

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express from "express"
+import routes from "./routes";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import session from "express-session";
+import passport from "./passport";
+dotenv.config();
+const app = express();
+const PORT = 3000
+
+app.use(express.json())
+// Session middleware configuration
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+// Initialize passport and session
+app.use(passport.initialize());
+app.use(passport.session());
+app.get("/", (req,res)=>{
+  res.send("Welcome")
+})
+
+app.use("/auth", routes)
+mongoose
+	.connect(process.env.MONGO_URI as string)
+	.then(() => {
+		app.listen(PORT, () =>
+			console.log(
+				`Server running on port ${PORT}`
+			)
+		);
+	})
+	.catch((err) =>
+		console.error(
+			"MongoDB connection error:",
+			err
+		)
+	);
+
